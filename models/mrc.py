e6d35d3bb53e876b43edede4a6dd2f3146e4e80e
cwszz <494542663@qq.com>
2020-04-06 02:57:10 +0800

    get final context representation

--- models/mrc.py
+++ models/mrc.py
@@ -5,6 +5,7 @@
 
 import torch
 from torch import nn
+from torch.nn import functional as F
 from torch.nn import CrossEntropyLoss, MSELoss
 
 from torch.autograd import Variable
@@ -24,8 +25,13 @@ class BertForBaiduQA_Answer_Selection(BertPreTrainedModel):
         self.lstmlayers = 1
         self.config = config
         # -----zhq
+        """Q-P match exactly Bi-attention"""
         self.lstm = nn.LSTM(input_size=config.hidden_size,hidden_size=config.hidden_size,
             num_layers=self.lstmlayers,bidirectional=True,batch_first=False)
+        self.score = nn.Linear(self.lstmlayers* 2 * 3 * config.hidden_size, 1)
+        # 2是双向，3是Bi-DAF （p,q,p·q）元素对应相乘
+        self.lstm_m = nn.LSTM(input_size=config.hidden_size*8, hidden_size=config.hidden_size*8 ,num_layers=1,
+            bidirectional=True,batch_first=True)
         # =======zhq
         self.qa_outputs = nn.Linear(config.hidden_size, 2)
         self.init_weights()
@@ -33,7 +39,7 @@ class BertForBaiduQA_Answer_Selection(BertPreTrainedModel):
     def forward(self, q_input_ids,  p_input_ids,q_attention_mask=None, q_token_type_ids=None, q_position_ids=None, q_head_mask=None,
                 p_attention_mask=None, p_token_type_ids=None, p_position_ids=None, p_head_mask=None,
                 start_positions=None, end_positions=None):
-
+        """Embedding"""
         p_outputs = self.bert(p_input_ids,
                             attention_mask=p_attention_mask,
                             token_type_ids=p_token_type_ids,
@@ -48,6 +54,7 @@ class BertForBaiduQA_Answer_Selection(BertPreTrainedModel):
         q_embedding = q_outputs[0] # size(batch,seq,hidden)
         p_embedding = p_embedding.transpose(0,1)
         q_embedding = q_embedding.transpose(0,1)
+        """Feature_Extraction"""
         # if self.use_cuda:
         #     h_0 = Variable(torch.zeros(self.layer_size, self.batch_size, self.hidden_size).cuda())
         #     c_0 = Variable(torch.zeros(self.layer_size, self.batch_size, self.hidden_size).cuda())
@@ -59,8 +66,49 @@ class BertForBaiduQA_Answer_Selection(BertPreTrainedModel):
         c_0_q = Variable(torch.zeros(self.lstmlayers*2, q_input_ids.size(0), self.config.hidden_size))
         h_0_p = Variable(torch.zeros(self.lstmlayers*2, p_input_ids.size(0), self.config.hidden_size)) # 乘2因为是双向
         c_0_p = Variable(torch.zeros(self.lstmlayers*2, p_input_ids.size(0), self.config.hidden_size))
-        p_features,(p_final_hidden_state, p_final_cell_state) = self.lstm(p_embedding,(h_0_p,c_0_p))
+        p_features,(p_final_hidden_state, p_final_cell_state) = self.lstm(p_embedding,(h_0_p,c_0_p)) # seq_length batch hidden
         q_features,(q_final_hidden_state, q_final_cell_state) = self.lstm(q_embedding,(h_0_q,c_0_q))
+        """Bi-Attention"""
+        u = torch.zeros(p_features.size(0),q_features.size(0),q_features.size(1))
+        # q_feature.size(1)其实就是batchsize
+        for t,p_word in enumerate(p_features):
+            for j,q_word in enumerate(q_features):
+                u[t][j] = self.score(torch.cat((p_word,q_word,q_word.mul(p_word)),1)).squeeze(1)  
+                #从第一维来合并，输入到线性层里。线性层的输入为batch * hidden_size
+        q2c_attention = F.softmax(u,1) # dim=1 表示行加和为1
+            # U_t = 对j叠加，a_(t,j)*U_j 所以j也就是query_length加和为1
+        c2q_attention = torch.max(u,1)[0] # dim = 0 表示从512个第0维中找出代表512个最大的
+        q2c_attention = q2c_attention.transpose(0,2).transpose(1,2)
+        c2q_attention = c2q_attention.transpose(0,1) 
+        q_features = q_features.transpose(0,1) #变成 22，64，1536
+        p_features = p_features.transpose(0,1) #变成 22, 512，1536
+        new_p_features_u = torch.zeros(q_features.size(0),q2c_attention.size(1),q_features.size(-1)) 
+            # 22, 512, 1536
+        # 构建加权
+        for i,(each_attn,each_q_word) in enumerate(zip(q2c_attention,q_features)):
+            new_p_features_u[i] = each_attn.mm(each_q_word)
+        # 获得q2c的加权特征
+        new_p_features_h = torch.zeros(p_features.size())
+        for i, (each_c2q,p_feature) in enumerate(zip(c2q_attention,p_features)):
+            for j,(c2q_weight,p_word_feature) in enumerate(zip(each_c2q,p_feature)):
+                new_p_features_h[i][j] = c2q_weight * p_word_feature
+        final_p_features = torch.zeros(p_features.size(0),p_features.size(1),p_features.size(2)*4)    
+        # final_p_features = torch.cat((p_features,new_p_features_h,p_features.mul(new_p_features_h),p_features.mul(new_p_features_u)),-1)  
+        # for i, (each_p,each_h,each_u) in enumerate(zip(p_features,new_p_features_h,new_p_features_u)):
+        #     for j, (each_p_word,each_h_word,each_u_word) in enumerate(zip(each_p,each_h,each_u)):
+        #         final_p_features[i][j] = torch.cat((each_p_word,each_u_word,
+        #             each_p_word.mul(each_u_word),each_p_word.mul(each_h_word)),0)
+        
+        # final_p_features2 = torch.zeros(p_features.size(0),p_features.size(1),p_features.size(2)*4)
+        final_p_features = torch.cat((p_features,new_p_features_h,p_features.mul(new_p_features_u),p_features.mul(new_p_features_h)),-1)  
+        # 构建最终的表示 [h;u ̃;h◦u ̃;h◦h ̃]
+        final_p_features =final_p_features.transpose(0,1)
+        h_0_p = Variable(torch.zeros(self.lstmlayers*2, final_p_features.size(0), self.config.hidden_size*8)) # 乘2因为是双向
+        c_0_p = Variable(torch.zeros(self.lstmlayers*2, final_p_features.size(0), self.config.hidden_size*8))
+        final_p_features,(p_final_hidden_state, p_final_cell_state) = self.lstm_m(final_p_features,(h_0_p,c_0_p)) # seq_length batch hidden
+        # new_p_features = torch.Tensor(new_p_features)  
+        print(new_p_features_h.size())
+        
         logits = self.qa_outputs(sequence_output)  # logits 是batch里所有的数据个数32 * 长度512 * (start and end) 2
         start_logits, end_logits = logits.split(1, dim=-1)       
         start_logits = start_logits.squeeze(-1)
